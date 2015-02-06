980b0ece58a1700cdc8e2e714366d53158b74bd5
Paul Fitzgerald <paulfitz99@gmail.com>
2015-02-06 13:45:18 +0000

    codejam practice

--- lib/practice.rb
+++ lib/practice.rb
@@ -1,1 +1,4 @@
 # Write your code below
+def reverse(string)
+  string.reverse
+end
--- spec/practise_spec.rb
+++ spec/practise_spec.rb
@@ -2,7 +2,9 @@ require('./lib/practice')
 
 describe('practise') do
 
-  it('') do
+  it('should reverse a string') do
+
+    expect(reverse("tring")).to eq "gnirt"
 
   end
   
